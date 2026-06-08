5b6adcd20781847b6eec14a01afd25b516f0dc8c
JesusManz <jesusmanzanares47@gmail.com>
2026-06-08 17:23:25 +0200

    fin merch

--- scripts/components.js
+++ scripts/components.js
@@ -64,10 +64,10 @@ async function initComponents() {
 
 // Mostrar popup
 // comentar bloque para ocultar el popup, descomentar para mostrarlo
- const popup = document.getElementById('popup');
-if (popup && !sessionStorage.getItem('popupClosed')) {
-  popup.style.display = 'flex';
-} 
+// const popup = document.getElementById('popup');
+//if (popup && !sessionStorage.getItem('popupClosed')) {
+// popup.style.display = 'flex';
+//} 
 
 
 
